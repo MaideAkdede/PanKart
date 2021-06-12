02fcd771c6dced7a9d64ee659f5aa49ce37e0f8e
MaideAkdede <maide.akdede@student.hepl.com>
2021-06-12 15:02:44 +0200

    :tada: initialize mix and packages

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,7 @@
 let mix = require('laravel-mix');
 
-mix.js('/wp-content/themes/pankart/resources/js/app.js', 'wp-content/themes/pankart/public/js')
-    .sass('/wp-content/themes/pankart/resources/sass/theme.scss', 'wp-content/themes/pankart/public/css');
+require('laravel-mix-pluton');
+
+mix.pluton('wp-content/themes/pankart/resources/js/parts')
+    .js('wp-content/themes/pankart/resources/js/app.js', 'wp-content/themes/pankart/public/js')
+    .sass('wp-content/themes/pankart/resources/sass/theme.scss', 'wp-content/themes/pankart/public/css');
--- wp-content/themes/pankart/resources/js/app.js
+++ wp-content/themes/pankart/resources/js/app.js
@@ -1,1 +1,14 @@
-console.log('App')
+import Pluton from "@whitecube/pluton/pluton";
+
+class EasySpacy {
+    constructor(){
+    }
+    load(){
+        this.pluton = new Pluton();
+        console.log('hello pluton here')
+    }
+}
+window.addEventListener('load', (e)=>{
+    window.EasySpacy = new EasySpacy();
+    window.EasySpacy.load();
+})
