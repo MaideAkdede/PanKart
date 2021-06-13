49c4534f42c6fb84bd8226573e35f40da7d75494
MaideAkdede <maide.akdede@student.hepl.com>
2021-06-13 02:55:10 +0200

    :zap: Add hover element JS files

--- wp-content/themes/pankart/resources/js/parts/test.js
+++ wp-content/themes/pankart/resources/js/parts/test.js
@@ -1,10 +1,19 @@
-export default class Test {
+export default class subMenu {
 
     static get selector() {
-
+        return '.sub-menu'
     }
 
-    constructor() {
-        console.log('test')
+    constructor(element) {
+        this.element = element;
+        this.element.classList.add('sub-menu__js');
+        this.element.classList.add('sub-menu__hidden');
+
+        this.btn = document.querySelector('.menu-item-44 a');
+
+        this.btn.addEventListener('click', (e)=>{
+            e.preventDefault();
+            this.element.classList.toggle('sub-menu__hidden')
+        });
     }
 }
